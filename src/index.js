690445467106c7d7672858aaee38a187cc8dfccb
mariafloscher <maffeloscher@gmail.com>
2021-01-29 17:40:24 +0100

    Forecast - first steps

--- src/index.js
+++ src/index.js
@@ -85,10 +85,31 @@ function showWeatherInfo(response) {
   );
 }
 
+function showForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.list[0];
+  forecastElement.innerHTML = `
+  <div class="col-sm-2">
+   <h2>17:00</h2>
+     <h3>
+       <span class="icon-today">
+        <img src="https://ssl.gstatic.com/onebox/weather/64/snow_light.png" class="forecast-icon"></i>
+       </span>
+      </h3>
+        <h4 class="highest-temperature">${Math.round(
+          forecast.main.temp_max
+        )}º</h4>
+        <h4 class="lowest-temperature">17º</h4>
+    </div>`;
+}
+
 function searchCity(city) {
   let apiKey = "e056cd661dc38600e82e2301e14e451d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeatherInfo);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function handleCitySubmit(event) {
