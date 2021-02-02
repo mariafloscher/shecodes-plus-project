7f2fc757e7291527fb77fa9acbd0c0256c501e38
mariafloscher <maffeloscher@gmail.com>
2021-02-02 10:41:59 +0100

    Fixed searchLocation function

--- src/index.js
+++ src/index.js
@@ -129,7 +129,10 @@ function searchLocation(position) {
   let lon = `${position.coords.longitude}`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeatherInfo);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
+
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
